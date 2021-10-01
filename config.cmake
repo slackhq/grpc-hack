bfb0f61d33ee15ba05ba8feb511a0b1133fb9123
Scott Sandler <ssandler@slack-corp.com>
2021-10-01 07:42:56 -0600

    add BOOST_INCLUDE_DIR as well

--- config.cmake
+++ config.cmake
@@ -1,6 +1,7 @@
 option(FORCE_TP_JEMALLOC "Always build and statically link jemalloc instead of using system version" OFF)
 option(JEMALLOC_INCLUDE_DIR "The include directory for built & statically linked jemalloc")
 option(DOUBLE_CONVERSION_INCLUDE_DIR "The include directory for built & statically linked double-conversion")
+option(BOOST_INCLUDE_DIR "The include directory for built & statically linked boost")
 
 if (FORCE_TP_JEMALLOC)
     # need to include statically-built third-party/jemalloc since the system version is <5 on 18.04
@@ -9,6 +10,10 @@ endif()
 
 if (NOT "$DOUBLE_CONVERSION_INCLUDE_DIR" STREQUAL "")
         include_directories(${DOUBLE_CONVERSION_INCLUDE_DIR})
+endif()
+
+if (NOT "$BOOST_INCLUDE_DIR" STREQUAL "")
+        include_directories(${BOOST_INCLUDE_DIR})
 endif()
 
 HHVM_ADD_INCLUDES(grpc_hack ./)
